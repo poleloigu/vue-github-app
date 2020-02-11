f3c62732ef2ac470970215755ba08dccc0b8fa27
Ole Loigu <ole.loigu@avalanchelabs.ee>
2020-02-11 20:56:21 +0200

    search: added initial search component - editorconfig and eslint fixes.

---
+++ src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <form v-on:submit.prevent="searchRepos(query)">
+      <input type="search" v-model="query" placeholder="Search repos" name />
+      <button type="submit">Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      // query: 'vk-projekt'  // Used for testing purposes
+      query: ''
+    };
+  },
+  methods: {
+    async searchRepos(query) {
+      const formattedQuery = query.split(/[ ,]+/).join(','); // Commas are used to separate values in query
+      const tail = `${formattedQuery}+in:name,description&type=Repositories`;
+      const url = `https://api.github.com/search/repositories?q=${tail}`;
+      const fetchData = fetch(url).then(response => response.json());
+      const responseRepositories = await fetchData;
+      console.log(responseRepositories);
+    }
+  }
+};
+</script>
